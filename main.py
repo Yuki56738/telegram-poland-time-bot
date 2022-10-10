5b0b9b474290601e8102826a13aa153afd2e6c83
Yuki <risaton@risaton.net>
2022-10-10 17:21:13 +0900

    commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import datetime
+import re
 
 import telegram.ext
 from telegram.ext import Updater
@@ -9,7 +10,10 @@ from telegram import *
 import logging
 from datetime import *
 import os
+import deepl
+from deepl import *
 
+DEEPL_API_KEY="7cdb289f-f521-70ca-e880-9725c281584f:fx"
 TOKEN = "5479025637:AAFun8ISKZsnCnnmqtRiN7hmOcLgTIki8F0"
 # from telegram.ext import Application
 
@@ -29,6 +33,7 @@ For a description of the Bot API, see this page: https://core.telegram.org/bots/
 # dispatcher = updater.dispatcher
 
 # def start(bot: ExtBot, update: Updater):
+transletor = deepl.Translator(DEEPL_API_KEY)
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -38,9 +43,14 @@ logging.basicConfig(
 
 async def start(update: Update, context: ContextTypes) -> None:
     user = update.effective_user
-    await update.message.reply_html(rf"Hi {user.mention_html()}!", reply_markup=ForceReply(selective=True))
+    await update.message.reply_html(rf"Hi {user.mention_html()}! Type sentence to translate!", reply_markup=ForceReply(selective=True))
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    # await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    msg = update.message.text
+    # msg2 = context.args
+    result = transletor.translate_text(msg, target_lang="EN-US")
+    result2 = transletor.translate_text(msg, target_lang="JA")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"{result.text}\n{result2.text}")
 async def ping(update:Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Pong!")
 async def poland(update: Update, ctx:ContextTypes.DEFAULT_TYPE):
@@ -53,25 +63,34 @@ async def poland(update: Update, ctx:ContextTypes.DEFAULT_TYPE):
 async def japan(update: Update, ctx:ContextTypes.DEFAULT_TYPE):
     dtNowJapan = datetime.now(timezone(timedelta(hours=9)))
     await ctx.bot.send_message(chat_id=update.effective_chat.id, text=str(dtNowJapan))
+async def trans(update: Update, ctx: ContextTypes.DEFAULT_TYPE):
+    msg = update.message.text
+    msg = re.sub("/trans ", "", msg)
+    result = transletor.translate_text(msg, target_lang="EN-US")
+    result2 = transletor.translate_text(msg, target_lang="JA")
+    await ctx.bot.send_message(chat_id=update.effective_chat.id, text=f"{result}\n{result2}")
 if __name__ == '__main__':
     # application = Bot(token="5479025637:AAHky8UZoT9EKrv5lRLELqjI-qs__ZsPxpo")
     app = Application.builder().token(TOKEN).build()
     # app = ApplicationBuilder().token("5479025637:AAHky8UZoT9EKrv5lRLELqjI-qs__ZsPxpo").build()
     # updater = Updater(TOKEN)
     # dp:Application = updater.dispatcher
-    start_handler = CommandHandler('start', start)
-    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    start_handler = CommandHandler('trans', trans)
+    # echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     ptime_handler = CommandHandler('poland', poland)
     ping_handler = CommandHandler('ping',ping)
     japan_handler = CommandHandler('japan', japan)
+    # message_handler = MessageHandler()
+    # trans_handler = CommandHandler('trans', trans)
     app.add_handler(start_handler)
-    app.add_handler(echo_handler)
+    # app.add_handler(echo_handler)
     app.add_handler(ping_handler)
     app.add_handler(ptime_handler)
     app.add_handler(japan_handler)
+    # app.add_handler(trans_handler)
     # app.run_polling()
     PORT = int(os.environ.get("PORT", "8443"))
-    # app.run_polling()
+    app.run_polling()
     # updater.start_polling()
     # updater.start_webhook(
     #     listen="0.0.0.0",
@@ -83,5 +102,5 @@ if __name__ == '__main__':
         listen="0.0.0.0",
         port=PORT,
         url_path=TOKEN,
-        webhook_url="https://telegram-webhook-yuki.herokuapp.com/" + TOKEN
+        webhook_url="https://tele-yuki.herokuapp.com/" + TOKEN
     )
